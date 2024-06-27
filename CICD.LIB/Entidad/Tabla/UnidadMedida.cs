627eef65a7f4dae24add4497a59512b1d3609c2d
Viqui-Alaro <31290294+Viqui-Alaro@users.noreply.github.com>
2024-06-27 15:09:30 -0400

    Update UnidadMedida.cs

--- CICD.LIB/Entidad/Tabla/UnidadMedida.cs
+++ CICD.LIB/Entidad/Tabla/UnidadMedida.cs
@@ -1,13 +1,15 @@
+using Dapper.Contrib.Extensions;
 using System;
 using System.Data;
 using System.Runtime.Serialization;
 
 namespace CICD.LIB.Entidad.Tabla
 {
-   [Table("GEN.UnidadMedida")]
+    [Table("GEN.UnidadMedida")]
     public class UnidadMedida
     {
         #region Atributos
+        [Key]
         public int? Id { get; set; }
         public string Nombre { get; set; }
         public string Abreviacion { get; set; }
