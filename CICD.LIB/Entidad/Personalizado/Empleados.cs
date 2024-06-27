8c716a47b504be21e0974c9e9eb825886b9800f3
Viqui-Alaro <31290294+Viqui-Alaro@users.noreply.github.com>
2024-06-27 15:17:58 -0400

    Add files via upload

---
+++ CICD.LIB/Entidad/Personalizado/Empleados.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CICD.LIB.Entidad.Personalizado
+{
+    public class Empleados
+    {
+        public int? Id { get; set; }
+        public string Nombre { get; set; }
+        public string ApellidoPaterno { get; set; }
+        public string ApellidoMaterno { get; set; }
+        public DateTime? FechaNacimiento { get; set; }
+        public string DescripcionSexo { get; set; }
+        public decimal? HaberBasico { get; set; }
+        public string NombreCargo { get; set; }
+    }
+}
---
+++ CICD.LIB/Entidad/Personalizado/Productos.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CICD.LIB.Entidad.Personalizado
+{
+    public class Productos
+    {
+        public int Id { get; set; }
+        public string Codigo { get; set; }
+        public string Nombre { get; set; }
+        public int StockMinimo { get; set; }
+        public string NombreMarca { get; set; }
+        public string NombreCategoria { get; set; }
+        public string DescripcionEstado { get; set; }
+    }
+}
