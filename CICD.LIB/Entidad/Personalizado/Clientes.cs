16dc8fd5b328b91739cfaaa0b3007b529d9dd0a7
VeronicaGoCa <programandoenlinea@gmail.com>
2024-06-27 16:12:04 -0400

    registro de clientes

---
+++ CICD.LIB/Entidad/Personalizado/Clientes.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CICD.LIB.Entidad.Personalizado
+{
+    public class Clientes
+    {
+        public int Id { get; set; }
+        public string DescripcionTipo { get; set; }
+        public string Nombre { get; set; }
+        public string Documento { get; set; }
+        public string Telefono { get; set; }
+        public string Correo { get; set; }
+        public string NombreContacto { get; set; }
+        public string DescripcionEstado { get; set; }
+    }
+}
--- CICD.WEB/Program.cs
+++ CICD.WEB/Program.cs
@@ -21,6 +21,7 @@ builder.Services.AddSingleton<IHttpContextAccessor, HttpContextAccessor>();
 builder.Services.AddScoped<CICD.LIB.Logica.GVarios>();
 builder.Services.AddScoped<CICD.LIB.Logica.GEmpleados>();
 builder.Services.AddScoped<CICD.LIB.Logica.GCargos>();
+builder.Services.AddScoped<CICD.LIB.Logica.GClientes>();
 
 IConfiguration Configuration = builder.Configuration;
 Conexion.Initialize(Configuration);
